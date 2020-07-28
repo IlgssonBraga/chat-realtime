1fe5e734b7bb468a6ed13316107328fd793fc5d1
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-28 10:44:11 -0300

    socket io connection

--- src/server.js
+++ src/server.js
@@ -1,14 +1,20 @@
 import express from 'express';
 import http from 'http';
 import path from 'path';
+import socketio from 'socket.io';
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server);
 
 app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
 app.get('/', (req, res) => {
   res.render('../public/index.html');
+});
+
+io.on('connection', () => {
+  console.log('New web socket connection');
 });
 
 server.listen(3333, () =>

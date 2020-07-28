258a91adcc324b36707ef8ae51e9462890db873c
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-28 10:29:20 -0300

    eslint prettier

--- src/server.js
+++ src/server.js
@@ -1,14 +1,16 @@
-import express from "express";
-import path from "path";
+import express from 'express';
+import http from 'http';
+import path from 'path';
 
 const app = express();
+const server = http.createServer(app);
 
-app.use(express.static(path.resolve(__dirname, "..", "public")));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
 
-app.get("/", (req, res) => {
-  res.render("../public/index.html");
+app.get('/', (req, res) => {
+  res.render('../public/index.html');
 });
 
-app.listen(3333, () =>
-  console.log("Server running on http://localhost:3333 ...")
+server.listen(3333, () =>
+  console.log('Server running on http://localhost:3333 ...'),
 );

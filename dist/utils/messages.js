11f4745b276bdd197e1f6f1c909edcbb28b28a52
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-08-01 16:02:22 -0300

    Sucrase

---
+++ dist/utils/messages.js
@@ -0,0 +1,17 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});const generateMessage = (username, text) => {
+  return {
+    username,
+    text,
+    createdAt: new Date().getTime(),
+  };
+};
+
+const generateLocationMessage = (username, url) => {
+  return {
+    username,
+    url,
+    createdAt: new Date().getTime(),
+  };
+};
+
+exports.generateMessage = generateMessage; exports.generateLocationMessage = generateLocationMessage;
